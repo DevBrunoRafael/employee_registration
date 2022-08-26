65889b7ad45b3d545f89e8bb14a37d0da1ce6a96
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-26 11:59:53 -0300

    CRUD department implemented

--- src/main/java/com/devbrunorafael/employee_registration/api/controller/DepartmentController.java
+++ src/main/java/com/devbrunorafael/employee_registration/api/controller/DepartmentController.java
@@ -1,15 +1,43 @@
 package com.devbrunorafael.employee_registration.api.controller;
 
+import com.devbrunorafael.employee_registration.domain.model.Department;
 import com.devbrunorafael.employee_registration.domain.service.DepartmentService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/departamento")
 @AllArgsConstructor
 public class DepartmentController {
 
     private DepartmentService departmentService;
+
+    @GetMapping("/{code}")
+    public Department findEmployee(@PathVariable String code){
+        return departmentService.findByCode(code);
+    }
+
+    @GetMapping
+    public List<Department> findAllEmployees(){
+        return departmentService.findAll();
+    }
+
+    @PostMapping
+    public Department registerEmployee(@RequestBody Department department){
+        return departmentService.save(department);
+    }
+
+    @PostMapping("/{code}")
+    public Department updateEmployee(@PathVariable String code, @RequestBody Department department){
+        department.setCode(code);
+        return departmentService.update(department);
+    }
+
+    @DeleteMapping("/{code}")
+    public void deleteEmployee(@PathVariable String code){
+        departmentService.deleteByCode(code);
+    }
 
 }
--- src/main/java/com/devbrunorafael/employee_registration/domain/service/DepartmentService.java
+++ src/main/java/com/devbrunorafael/employee_registration/domain/service/DepartmentService.java
@@ -1,9 +1,35 @@
 package com.devbrunorafael.employee_registration.domain.service;
 
+import com.devbrunorafael.employee_registration.domain.model.Department;
+import com.devbrunorafael.employee_registration.domain.repository.DepartmentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    public Department findByCode(String code) {
+        return departmentRepository.findById(code).get();
+    }
+
+    public List<Department> findAll() {
+        return departmentRepository.findAll();
+    }
+
+    public Department save(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    public Department update(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    public void deleteByCode(String code) {
+        departmentRepository.deleteById(code);
+    }
 }
