976f9c57fe5a058324c5228393c521a80aee9ba9
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-09-16 15:39:17 -0300

    implementing access authorizations to routes and exchanging responses from endpoints

--- src/main/java/com/devbrunorafael/employee_registration/api/controller/DepartmentController.java
+++ src/main/java/com/devbrunorafael/employee_registration/api/controller/DepartmentController.java
@@ -5,38 +5,65 @@ import com.devbrunorafael.employee_registration.domain.model.Employee;
 import com.devbrunorafael.employee_registration.domain.service.DepartmentService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/departamento")
 @AllArgsConstructor
 public class DepartmentController {
 
     private DepartmentService departmentService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}/empregados")
-    public List<Employee> findDepartmentEmployees(@PathVariable Long id){
-        return departmentService.findEmployees(id);
+    public ResponseEntity<Object> findDepartmentEmployees(@PathVariable(name = "id") Long id){
+        List<Employee> employees = departmentService.findEmployees(id);
+
+        if (employees.size() == 0){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body("Não há funcionários cadastrados nesse departamento!");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(employees);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public Department findDepartment(@PathVariable Long id){
-        return departmentService.findDepartmentById(id);
+    public ResponseEntity<Object> findDepartment(@PathVariable(name = "id") Long id) {
+        Optional<Department> department = departmentService.findDepartmentById(id);
+
+        if (department.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Departamento ".concat(String.valueOf(id)).concat(" não encontrado!"));
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(department.get());
     }
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<Department> findAllDepartments(){
-        return departmentService.findDepartments();
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("all")
+    public ResponseEntity<Object> findAllDepartments(){
+        List<Department> departments = departmentService.findDepartments();
+
+        if (departments.size() == 0) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Não há departamentos cadastrados");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(departments);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/cadastrar")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Department registerDepartment(@RequestBody Department department){
-        return departmentService.saveDepartment(department);
+    public ResponseEntity<Object> registerDepartment(@RequestBody Department department){
+
+        if (departmentService.exists(department.getName())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("O departamento já existe!");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(departmentService.saveDepartment(department));
     }
 
 }
